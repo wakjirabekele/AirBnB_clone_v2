fe7105706943596372eb2f6f4fb234d5babe573e
motirase <117908555+motirase@users.noreply.github.com>
2023-06-24 09:54:22 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines the User class"""
+"""Defines the User class."""
 from models.base_model import Base
 from models.base_model import BaseModel
 from sqlalchemy import Column
